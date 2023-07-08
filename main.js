00b3028a0c90e13a81d90a4b41e8ce117d562dc1
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-08 15:31:23 +0530

    adding nodemon script in package.json

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const {app, BrowserWindow} = require('electron')
 
+process.env.NODE_ENV = 'development';
 
-function createMainWindow() {
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+function createWindow() {
     const mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: 500,
         height: 600,
         icon: './assets/Icon_256x256.png',
+        resizable: isDev ? true : false,
     })
     // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     mainWindow.loadFile('./app/index.html')
 }
 
 
-app.on('ready', createMainWindow)
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+      })
+})
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
