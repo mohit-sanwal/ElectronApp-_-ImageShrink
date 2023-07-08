137e9ed692cf74131177c133aea379edaf7a6af4
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-08 16:09:34 +0530

    menu implementation in electron

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 process.env.NODE_ENV = 'development';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform == 'darwin' ? true : false;
+
+let mainWindow;
 function createWindow() {
-    const mainWindow = new BrowserWindow({
+     mainWindow = new BrowserWindow({
         title: 'ImageShrink',
         width: 500,
         height: 600,
@@ -18,12 +21,33 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow();
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu);
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
           createWindow()
         }
       })
 })
+
+const menu = [
+    ...(isMac ? [{role: 'appMenu'}]: []),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+                accelerator: 'CmdOrCtrl+W',
+                click: () => app.quit()
+            },
+            {
+                label: 'stay',
+                click: () => app.quit()
+            }
+        ]
+    }
+]
 
 
 app.on('window-all-closed', () => {
